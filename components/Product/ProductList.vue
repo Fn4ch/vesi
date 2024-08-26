0223341f3197ef7197e5692ae3e524030c89a946
Farhad <farhadmainsot@gmail.com>
2024-08-26 12:52:34 +0300

    reworked product list

--- components/Product/ProductList.vue
+++ components/Product/ProductList.vue
@@ -5,10 +5,15 @@
             <h4>Широкий выбор для всех случаев</h4>
         </div>
         <section class="products-list">
-            <ProductCard v-for="(p, index) in PRODUCTS_DATA" :image="p.image" :title="p.title" :description="p.description" :class="index % 2 === 1 ? 'products-list__left' : 'products-list__right'"/>
+            <ProductCard 
+            v-for="(p, index) in PRODUCTS_DATA" 
+            :image="p.image" 
+            :title="p.title" 
+            :description="p.description" 
+            :class="index % 2 === 1 ? 'products-list__left' : 'products-list__right'"/>
         </section>
     </article>
-</template>
+</section></template>
 <script setup lang="ts">
 import {PRODUCTS_DATA} from '@/src/shared/constants/PRODUCTS'
 
